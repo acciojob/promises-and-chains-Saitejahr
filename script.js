bdee05b0230e2b48d212144a953fd71f312566d8
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-03-22 20:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const btn = document.getElementById("btn");
+
+            btn.addEventListener('click', run);
+
+            // console.log(name, age)
+
+            function checkAge() {
+                const age = document.getElementById('age').value;
+                const name = document.getElementById('name').value;
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        if (age >= 18) {
+                            resolve("Welcome "+ name +" You can vote")
+                        }
+                        else {
+                            reject("Sorry "+ name +" You can not vote");
+                        }
+                    }, 4000);
+
+                })
+
+
+            }
+
+           function run(){
+                 checkAge().then((res) => {
+                alert(res);
+            })
+                .catch((err) => {
+                    alert(err);
+                });
+            }
